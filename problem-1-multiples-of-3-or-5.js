071d606963f0ac6874786f2b6736160d5090fff8
eleegance <89720100+eleegance@users.noreply.github.com>
2021-09-01 21:27:26 +1000

    Added problem description and link

--- problem-1-multiples-of-3-or-5.js
+++ problem-1-multiples-of-3-or-5.js
@@ -1,3 +1,5 @@
+/* Problem: Find the sum of all the multiples of 3 or 5 below 1000.
+https://projecteuler.net/problem=1 */
 const sumOfMultiples = (num) => {
   let sumOf = 0;
   for (let i = 1; i < num; i++) {
