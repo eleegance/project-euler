bad813be169b922f122b1a7340366b9ce4a2442e
eleegance <89720100+eleegance@users.noreply.github.com>
2021-09-29 12:28:46 +1000

    Create problem-3-largest-prime-factor.js

---
+++ problem-3-largest-prime-factor.js
@@ -0,0 +1,30 @@
+//Problem: What is the largest prime factor of 600851475143?
+const primeCheckerFunction = (n) => {
+  if (n === 1) {
+    return "undefined";
+  }
+  else if (n === 2) {
+    return true;
+  }
+  else {
+    for (let i = 2; i < n; i++) {
+      if (n % i === 0) {
+          return false;
+        }
+    }
+    return true;
+  }
+};
+
+const maxPrimeFactoriser = (n) => {
+  var factorisationArray = [];
+  for (let i = 193387; i >= 2; i -= 2) {
+    if (n % i === 0 && primeCheckerFunction(i) == true) {
+      factorisationArray.push(i);
+      break;
+    }
+  }
+  return factorisationArray;
+}
+console.log(maxPrimeFactoriser(600851475143))
+//Output: 6857
